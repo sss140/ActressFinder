f74a0cc63f6e682465d0fb7e0519efa3d1b79583
佐藤一成 <kazunarisato0309@gmail.com>
2021-01-02 17:25:18 +0900

    Initial Commit

---
+++ DMM_Actress_Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DMM_Actress_Finder
+//
+//  Created by 佐藤一成 on 2021/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DMM_Actress_Finder/DMM_Actress_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DMM_Actress_FinderApp.swift
+//  DMM_Actress_Finder
+//
+//  Created by 佐藤一成 on 2021/01/02.
+//
+
+import SwiftUI
+
+@main
+struct DMM_Actress_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
