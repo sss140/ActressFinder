b37d99980fd80c1d8b5a8fc0d84c3329b1397702
佐藤一成 <kazunarisato0309@gmail.com>
2021-01-02 20:38:48 +0900

    01022100

--- DMM_Actress_Finder/EnvironmentObjects/EnvironmentURLManager.swift
+++ DMM_Actress_Finder/EnvironmentObjects/EnvironmentURLManager.swift
@@ -8,8 +8,27 @@
 import Foundation
 
 class EnvironmentURLManager:ObservableObject{
-    let str:String = "lllll"
-    //Picker
+    
+    var requestParameter:Dictionary<String,String> = Dictionary<String,String>()
+    
+    //Bust
+    @Published var bustValue:CountableClosedRange = 70...100
+    @Published var bustAppearance:Bool = false
+    @Published var bustString:String = "バスト70cm〜100cm"
+    //waist 47-135
+    @Published var waistValue:CountableClosedRange = 60...80
+    @Published var waistAppearance:Bool = false
+    @Published var waistString:String = "ウエスト60cm〜80cm"
+    //Hip 50-150
+    @Published var hipValue:CountableClosedRange = 60...80
+    @Published var hipAppearance:Bool = false
+    @Published var hipString:String = "ヒップ60cm〜80cm"
+    //height 130-205
+    @Published var heightValue:CountableClosedRange = 140...180
+    @Published var heightAppearance:Bool = false
+    @Published var heightString:String = "身長140cm〜180cm"
+    
+    //sort Picker
     @Published var sortValue:String
     @Published var sortAppearance:Bool = false
     let sort:Dictionary<String,String> = ["名前昇順":"name","名前降順":"-name","バスト昇順":"bust","バスト降順":"-bust","ウエスト昇順":"waist","ウエスト降順":"-waist","ヒップ昇順":"hip","ヒップ降順":"-hip","身長昇順":"height","身長降順":"-height","生年月日昇順":"birthday","生年月日降順":"-birthday","女優ID昇順":"id","女優ID降順":"-id"]
@@ -17,7 +36,75 @@ class EnvironmentURLManager:ObservableObject{
     
     
     init(){
+        requestParameter["api_id"] = "J0Zp5Xx6C50uw9cgZrqu"
+        requestParameter["affiliate_id"] = "s140s140-990"
+        requestParameter["output"] = "json"
+        requestParameter["hits"] = "20"
+        
+        
+        
+        
         sortString = sort.map{$0.key}.sorted()
         sortValue = sortString.first!
+        
+        
+        
+    }
+    
+    
+    
+    
+    func getURL(){
+        var url:String = "https://api.dmm.com/affiliate/v3/ActressSearch?"
+        //sort
+        requestParameter["sort"] = (self.sortAppearance) ? sort[self.sortValue]:""
+        //bust
+        if self.bustAppearance{
+            let max = String(Int(self.bustValue.upperBound))
+            requestParameter["lte_bust"] = max
+            let min = String(Int(self.bustValue.lowerBound))
+            requestParameter["gte_bust"] = min
+        }else{
+            requestParameter["gte_bust"] = ""
+            requestParameter["lte_bust"] = ""
+        }
+        //waist
+        if self.waistAppearance{
+            let max = String(Int(self.waistValue.upperBound))
+            requestParameter["lte_waist"] = max
+            let min = String(Int(self.waistValue.lowerBound))
+            requestParameter["gte_waist"] = min
+        }else{
+            requestParameter["gte_waist"] = ""
+            requestParameter["lte_waist"] = ""
+        }
+        //hip
+        if self.hipAppearance{
+            let max = String(Int(self.hipValue.upperBound))
+            requestParameter["lte_hip"] = max
+            let min = String(Int(self.hipValue.lowerBound))
+            requestParameter["gte_hip"] = min
+        }else{
+            requestParameter["gte_hip"] = ""
+            requestParameter["lte_hip"] = ""
+        }
+        //height
+        if self.heightAppearance{
+            let max = String(Int(self.heightValue.upperBound))
+            requestParameter["lte_height"] = max
+            let min = String(Int(self.heightValue.lowerBound))
+            requestParameter["gte_height"] = min
+        }else{
+            requestParameter["gte_height"] = ""
+            requestParameter["lte_height"] = ""
+        }
+        
+        
+        
+        //
+        let parameters = requestParameter
+            .compactMap { (key, value) in (value == "") ? nil:key + "=" + value}
+        url += parameters.joined(separator: "&")
+        print(url)
     }
 }
--- DMM_Actress_Finder/Views/ConfigSheet.swift
+++ DMM_Actress_Finder/Views/ConfigSheet.swift
@@ -6,18 +6,84 @@
 //
 
 import SwiftUI
+import Sliders
 
 struct ConfigSheet: View {
     @EnvironmentObject var urlManager:EnvironmentURLManager
     @Environment(\.presentationMode) var presentationMode
+    
+    
     
     var body: some View {
         NavigationView{
             VStack{
                 List{
                     VStack{
+                        Button(action: {
+                            self.urlManager.getURL()
+                        }){
+                            Text("Button")
+                        }
+                    }
+                    //bust
+                    VStack{
+                        Toggle(isOn: self.$urlManager.bustAppearance){
+                            Text(self.urlManager.bustAppearance ? self.urlManager.bustString:"バスト")
+                        }
+                        if self.urlManager.bustAppearance{
+                        RangeSlider(range: self.$urlManager.bustValue, in: 50...155, step: 1, onEditingChanged: {_ in
+                            let max:Int = Int(self.urlManager.bustValue.upperBound)
+                            let min:Int = Int(self.urlManager.bustValue.lowerBound)
+                            self.urlManager.bustString = "バスト \(min)cm〜\(max)cm"
+                        })
+                        
+                        }
+                    }
+                    //waist
+                    
+                    VStack{
+                        Toggle(isOn: self.$urlManager.waistAppearance){
+                            Text(self.urlManager.waistAppearance ? self.urlManager.waistString:"ウエスト")
+                        }
+                        if self.urlManager.waistAppearance{
+                            RangeSlider(range: self.$urlManager.waistValue, in: 45...140, step: 1, onEditingChanged: {_ in
+                                let max:Int = Int(self.urlManager.waistValue.upperBound)
+                                let min:Int = Int(self.urlManager.waistValue.lowerBound)
+                            self.urlManager.waistString = "ウエスト \(min)cm〜\(max)cm"
+                        })
+                        }
+                    }
+                   //Hip
+                    VStack{
+                        Toggle(isOn: self.$urlManager.hipAppearance){
+                            Text(self.urlManager.hipAppearance ? self.urlManager.hipString:"ヒップ")
+                        }
+                        if self.urlManager.hipAppearance{
+                            RangeSlider(range: self.$urlManager.hipValue, in: 25...150, step: 1, onEditingChanged: {_ in
+                                let max:Int = Int(self.urlManager.hipValue.upperBound)
+                                let min:Int = Int(self.urlManager.hipValue.lowerBound)
+                            self.urlManager.hipString = "ヒップ \(min)cm〜\(max)cm"
+                        })
+                        }
+                    }
+                    //height
+                    VStack{
+                        Toggle(isOn: self.$urlManager.heightAppearance){
+                            Text(self.urlManager.heightAppearance ? self.urlManager.heightString:"身長")
+                        }
+                        if self.urlManager.heightAppearance{
+                            RangeSlider(range: self.$urlManager.heightValue, in: 130...205, step: 1, onEditingChanged: {_ in
+                                let max:Int = Int(self.urlManager.heightValue.upperBound)
+                                let min:Int = Int(self.urlManager.heightValue.lowerBound)
+                                self.urlManager.heightString = "身長 \(min)cm〜\(max)cm"
+                        })
+                        }
+                    }
+                    
+                    //sort
+                    VStack{
                         Toggle(isOn: self.$urlManager.sortAppearance){
-                            Text("ソート順")
+                            Text("ソート")
                         }
                         if self.urlManager.sortAppearance{
                             Picker(selection: self.$urlManager.sortValue, label: Text("Sort")){
@@ -43,3 +109,4 @@ struct ConfigSheet_Previews: PreviewProvider {
         ConfigSheet()
     }
 }
+
